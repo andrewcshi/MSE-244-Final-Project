fe216a4a5f0df5f8fbadb50978a937c78a4c64a9
Abhishek Vangipuram <abhivangipuram@gmail.com>
2024-06-02 21:37:44 -0700

    added metrics

--- metrics.py
+++ metrics.py
@@ -1,7 +1,29 @@
-SHOULD CALCULATE FOLLOWING METRICS FOR ALL STRATEGIES
+import pandas as pd
+import numpy as np
 
-- SHARPE
-- MAX DRAWDOWN
-- MEAN RETURN
-- VARIANCE / VOLATILITY
-- CUMULATIVE RETURN
+def drawdowns(returns: pd.Series) -> pd.Series:
+    cr = ((1 + returns).cumprod())
+    rm = cr.expanding(min_periods=1).max()
+    ddown = (cr / rm) - 1
+    return ddown
+
+def cumulative_returns(returns: pd.Series) -> pd.Series:
+    return (1 + returns).cumprod() - 1
+
+def annualized_sharpe_ratio(returns: pd.Series, risk_free_rate: float = 0.0) -> pd.Series:
+    return returns.mean() / returns.std() * np.sqrt(252)
+
+def annualized_return(returns: pd.Series) -> pd.Series:
+    return returns.mean() * 252
+
+def annualized_volatility(returns: pd.Series) -> pd.Series:
+    return returns.std() * np.sqrt(252)
+
+def skewness(returns: pd.Series) -> pd.Series:
+    return returns.skew()
+
+def kurtosis(returns: pd.Series) -> pd.Series:
+    return returns.kurt()
+
+def max_drawdown(returns: pd.Series) -> pd.Series:
+    return drawdowns(returns).min()
